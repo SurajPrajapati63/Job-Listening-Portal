d734b7a0cc98c5edc2f70d7ab9584b810b95745e
SurajPrajapati63 <prajapatisuraj88409@gmail.com>
2024-10-05 23:46:45 +0530

    Add files via upload

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { useDispatch } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import { useNavigate } from 'react-router-dom';
+
+const HeroSection = () => {
+    const [query, setQuery] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const searchJobHandler = () => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+    }
+
+    return (
+        <div className='text-center'>
+            <div className='flex flex-col gap-5 my-10'>
+                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[green]'>Dream Jobs</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur temporibus nihil tempora dolor!</p>
+                <div className='flex w-[40%] shadow-lg border border-green-200 pl-3  items-center gap-4 mx-auto bg-white rounded-sm'>
+                    <input
+                        type="text"
+                        placeholder='Find your dream jobs'
+                        onChange={(e) => setQuery(e.target.value)}
+                        className='outline-none border-none w-full'
+
+                    />
+                    <Button onClick={searchJobHandler} className=" bg-[green]">
+                        <Search className='h-5 w-5' />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
